68efb4ee8471cfddf17554b910f5010a6ef930e6
Caleb Han <calebhan08js@gmail.com>
2024-01-08 18:29:30 +0800

    added chart.py

---
+++ chart.py
@@ -0,0 +1,20 @@
+from pyflowchart import *
+
+st = StartNode('a_pyflow_test')
+op = OperationNode('do something')
+cond = ConditionNode('Yes or No?')
+io = InputOutputNode(InputOutputNode.OUTPUT, 'something...')
+sub = SubroutineNode('A Subroutine')
+e = EndNode('a_pyflow_test')
+
+sub.set_connect_direction("right")
+    
+st.connect(op)
+op.connect(cond)
+cond.connect_yes(io)
+cond.connect_no(sub)
+sub.connect(op)
+io.connect(e)
+ 
+fc = Flowchart(st)
+print(fc.flowchart())
